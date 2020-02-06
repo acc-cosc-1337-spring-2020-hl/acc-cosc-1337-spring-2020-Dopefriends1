42618c2930fe2f539ea84ad32fe02f780438b3ae
gomezbrianacc <brian.gomez@g.austincc.edu>
2020-02-05 19:10:50 -0600

    Unfished Classwork Assignment 2 (what i had so far)

--- src/examples/02_module/03_switch/main.cpp
+++ src/examples/02_module/03_switch/main.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
+#include "switch.h"
+
+using std::cin; using std::cout;
 
 int main() 
 {
-	
+	int option;
+
+	cout << "Enter a number between 1-4: ";
+	cin >> option;
+
+	string result = menu(option);
+
+	cout << result;
+
 
 	return 0;
 }
--- src/examples/02_module/03_switch/switch.h
+++ src/examples/02_module/03_switch/switch.h
@@ -1,8 +1,12 @@
 //write string include
 #include<string>
 
+using std::string; //used this on here so we dont have to repeat it and define what a switch is
+					//on the main function
+
 //Write prototype for function that accepts a num and returns a string
 
+string menu(int num);
 
 
 
